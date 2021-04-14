0dd2796344f505d45fc555c98f87b0a0c16e35c5
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-04-14 09:18:40 +0800

    Create components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+import Home from "./components/home";
+import Posts from "./components/posts";
+import Profile from "./components/profile";
 
 const App = () => {
-    return <div>Hello World</div>;
+    return (
+        <BrowserRouter>
+            <header>Header</header>
+            <Route path="/" exact component={Home} />
+            <Route path="/posts" exact component={Posts} />
+            <Route path="/profile" exact component={Profile} />
+        </BrowserRouter>
+    );
 };
 
-ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
